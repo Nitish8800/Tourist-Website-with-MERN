c5d59b7c7618e919e258da9652cec325b538c8ca
Nitish8800 <nitishfea12-784@gmail.com>
2022-07-28 21:44:36 +0530

    user signin

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -10,16 +10,20 @@ export const signin = async (req, res) => {
 
   try {
     const oldUser = await UserModal.findOne({ email });
-    if (!oldUser)
-      return res.status(404).json({ message: "User doesn't exist" });
+    if (!oldUser){
+        return res.status(404).json({ message: "User doesn't exist" })
+    }
+      
 
     const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
-    if (!isPasswordCorrect)
-      return res.status(400).json({ message: "Invalid credentials" });
+    if (!isPasswordCorrect){
+        return res.status(400).json({ message: "Invalid credentials" })
+    }
+      
 
     const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
-      expiresIn: "1h",
+      expiresIn: "90d",
     });
 
     res.status(200).json({ result: oldUser, token });
