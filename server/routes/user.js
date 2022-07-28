36df3cbd56c393a75b07b131ca4f363c1f8abb3c
Nitish8800 <nitishfea12-784@gmail.com>
2022-07-28 21:47:00 +0530

    user signin

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,7 +5,7 @@ import { users, signup, signin, googleSignIn } from "../controllers/user.js";
 
 router.post("/signup", signup);
 router.get("/", users);
-// router.post("/signin", signin);
+router.post("/signin", signin);
 // router.post("/googleSignIn", googleSignIn);
 
 export default router;
